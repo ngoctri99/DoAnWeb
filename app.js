0f3698dc8c8dfd0e12ef9f267903efbb1eae767c
Ngọc Trí <ngoctri990705@gmail.com>
2020-07-01 00:00:01 +0700

    Cấu trúc thư mục

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var createError = require('http-errors');
 var express = require('express');
+const expbs = require('express-handlebars');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+app.set('view engine', 'hbs');
+app.engine('hbs', expbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views/layout')
+}));
+
+hbs.registerPartials(__dirname + '/views/partials', function(err){});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/home', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'home' });
+});
 module.exports = router;
