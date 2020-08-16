7bf83bd00d652b2afe850bb02b84a52dabd4398c
zkutamz <tujiutujngoxtujkhoxjaj@gmail.com>
2020-08-17 02:24:09 +0700

    thêm tìm kiếm với nhãn và các bài viết liên quan

--- routes/accounts.js
+++ routes/accounts.js
@@ -26,10 +26,4 @@ router.post('/quenpass', accountController.xlindexquenpass);
 router.get('/xlquenpass', accountController.indexxlquenpass);
 router.post('/xlquenpass', accountController.xlquenpass);
 
-router.get('/baivietcuaminh', accountController.indexbaivietcuaminh);
-
-router.get('/suabaiviet', accountController.suabaiviet);
-router.post('/suabaiviet', accountController.xlsuabaiviet);
-
-
 module.exports = router;
