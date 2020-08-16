a1259b0a678e461b4819dc56bdf0a6537010e2a6
zkutamz <tujiutujngoxtujkhoxjaj@gmail.com>
2020-08-17 03:51:21 +0700

    thêm mỗi chuyên mục 1 bài viết mới

--- models/posts.js
+++ models/posts.js
@@ -32,7 +32,7 @@ module.exports = {
       return db.load(sql);
     },
     getHot: function(limit = 3){
-      var sql = "SELECT p1.*,c.category_name FROM (posts as p1 JOIN comments as c1 ON p1.post_id = c1.post_id) join categories c ON c.category_id = p1.post_cate and DATEDIFF( CURDATE(), p1.post_browse ) <= 7 "+
+      var sql = "SELECT p1.*,c.category_name FROM (posts as p1 JOIN comments as c1 ON p1.post_id = c1.post_id) join categories c ON c.category_id = p1.post_cate and p1.post_status = 1 and DATEDIFF( CURDATE(), p1.post_browse ) <= 7 "+
       " GROUP BY p1.post_id "+
        " ORDER BY COUNT(*) DESC, p1.post_browse DESC LIMIT 0,"+limit;
       return db.load(sql);
@@ -48,10 +48,10 @@ module.exports = {
 
     getPostWithCategory: function(categoriesParent = 0,categoriesSub = 0){
       if(categoriesParent != 0){
-        return db.load(`select * from categories c, posts p where c.category_id = p.post_cate and (c.category_id = ${categoriesParent} or c.category_cate = ${categoriesParent}) order by p.post_browse DESC `);
+        return db.load(`select * from categories c, posts p where c.category_id = p.post_cate and p.post_status = 1 and (c.category_id = ${categoriesParent} or c.category_cate = ${categoriesParent}) order by p.post_browse DESC `);
       }
       if(categoriesSub != 0){
-        return db.load(`select * from categories c, posts p where c.category_id = p.post_cate and c.category_id = ${categoriesSub} order by p.post_browse DESC`);
+        return db.load(`select * from categories c, posts p where c.category_id = p.post_cate and p.post_status = 1 and c.category_id = ${categoriesSub} order by p.post_browse DESC`);
       }
     },
 
@@ -67,6 +67,6 @@ module.exports = {
       return db.load(`SELECT DISTINCT post_cate from posts ORDER by post_browse DESC, post_views DESC LIMIT 0, ${limit}`);
     },
     getPostNewByCategory(category_id,limit =1){
-      return db.load(`SELECT p.*, c.category_name from posts p join categories c on p.post_cate = c.category_id where post_cate = ${category_id} and p.post_status = 1 ORDER by p.post_browse DESC, p.post_level DESC,p.post_views DESC LIMIT 0, ${limit}`);
+      return db.load(`SELECT p.*, c.category_name from posts p join categories c on p.post_cate = c.category_id where p.post_cate = ${category_id} and p.post_status = 1 ORDER by p.post_browse DESC, p.post_level DESC, p.post_views DESC LIMIT 0, ${limit}`);
     }
   };
